6a86d7a2f1437c036d189a5d63803b658e69ba61
albrektsson <emil.albrektsson@nav.no>
2026-05-07 12:02:11 +0200

    add team as a prefix in the prio list

--- src/app/modules/vulnerabilities/components/VulnerabilitiesToLookAt.tsx
+++ src/app/modules/vulnerabilities/components/VulnerabilitiesToLookAt.tsx
@@ -167,7 +167,7 @@ const VulnerabilitiesToLookAt = ({ bucketName, minThreshold, maxThreshold, selec
                                 <Accordion.Header>
                                     <HStack gap="space-8" align="center" justify="space-between" style={{ width: "100%" }}>
                                         <BodyShort weight="semibold">
-                                            {workloadGroup.workload.name} {t("list.vulnerabilitiesInWorkload", { count: workloadGroup.vulnerabilities.length })}
+                                            {workloadGroup.team}/{workloadGroup.workload.name} {t("list.vulnerabilitiesInWorkload", { count: workloadGroup.vulnerabilities.length })}
                                         </BodyShort>
                                         <HStack gap="space-8" align="center">
                                             {workloadGroup.workload.repository && (
