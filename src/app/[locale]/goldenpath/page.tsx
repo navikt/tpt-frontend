85b8132b82a1e6c4608cc91d0b01354ac1a7ae20
J-K. Solbakken <jan.kare.solbakken@nav.no>
2026-08-28 15:13:04 +0200

    Display repos grouped by if they have issues

--- src/app/[locale]/goldenpath/page.tsx
+++ src/app/[locale]/goldenpath/page.tsx
@@ -3,14 +3,15 @@
 import { useEffect, useState } from "react";
 
 export default function GoldenPathPage() {
-  const [checkResults, setCheckResults] = useState(null)
+  const empty: RepoWithGroupedChecks[] = []
+  const [checkResults, setCheckResults] = useState(empty)
   const [isLoading, setLoading] = useState(true)
 
   useEffect(() => {
     fetch("/api/datacollector/checks")
       .then((res) => res.json())
-      .then((checks) => {
-        setCheckResults(checks)
+      .then((backendReply) => {
+        setCheckResults(groupedChecks(backendReply))
         setLoading(false)
       })
   }, [])
@@ -19,49 +20,53 @@ export default function GoldenPathPage() {
   if (!checkResults) return <p>Nothing to see here</p>
 
   return <section>
-    <div id="check-results">{format(checkResults)}</div>
+    <div id="check-results">{display(checkResults)}</div>
   </section>;
 }
 
-
-const groupedChecks = (fetchResult: RepoChecks) =>
-  Object.keys(fetchResult).map(repoName => {
+const groupedChecks = (backendReply: RepoChecks) => {
+  return Object.keys(backendReply).map(repoName => {
     const repo: RepoWithGroupedChecks = {
       name: repoName,
       good: [],
       bad: []
     }
-    const results = fetchResult[repoName]
+    const results = backendReply[repoName]
     results?.forEach(result => {
-      if (result.reasons) { repo.bad.push(result) } else { repo.good.push(result) }
+      if ('reasons' in result) { repo.bad.push(result) } else { repo.good.push(result) }
     })
     return repo
   })
+}
 
-const format = (fetchResult: RepoChecks) => {
-  const grouped = groupedChecks(fetchResult)
-  return (<div>
-    {grouped.map(g =>
-      <details  key={g.name}>
+const display = (checks: RepoWithGroupedChecks[]) => { 
+  const allGoodRepos = checks.filter(c => c.bad.length === 0)
+  const reposWithIssues = checks.filter(c => c.bad.length !== 0)
+  return <div>
+    <h2>Repos with issues</h2>
+    {reposWithIssues.map(g =>
+      <details key={g.name}>
         <summary>{g.name} ({g.bad.length})</summary>
-        <ul>{g.bad.map(b => <li key="{b.name}">{b.reasons?.join()}</li>)}</ul>
+        <ul>{g.bad.map(b => <li key={b.name}>{b.severity} - {b.reasons?.join()}</li>)}</ul>
         <p>Nr of good checks: {g.bad.length}</p>
       </details>)
     }
-  </div>)
+    <h2>Repos with no issues</h2>
+    <p>{allGoodRepos.map(r => r.name).join()}</p>
+  </div>
 }
 
 
 type RepoName = string
 
 type CheckResult = {
-    type: "AllGood" | "NeedsWork";
-    name: string;
-    desc: string;
-    severity: "LOW" | "MEDIUM" | "HIGH" | "UNKNOWN";
-    whenChecked: string;
-    reasons?: string[];
-  }
+  type: "AllGood" | "NeedsWork";
+  name: string;
+  desc: string;
+  severity: "LOW" | "MEDIUM" | "HIGH" | "UNKNOWN";
+  whenChecked: string;
+  reasons?: string[];
+}
 
 type RepoChecks = Record<RepoName, CheckResult[]>
 
